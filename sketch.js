e5d7cecac43290291357cc696538dbd113c61e64
Mazzzzc <72050517+Mazzzzc@users.noreply.github.com>
2021-08-15 17:38:08 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+//Create variables here
+var fedTime;
+var lastFed;
+var foodObj;
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
